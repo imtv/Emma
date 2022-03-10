717851a6e30cd3a5c47980fb1bed955aaeb65079
imtv <59756626+imtv@users.noreply.github.com>
2022-03-10 12:57:50 +0800

    Update Xray.sh

--- Xray.sh
+++ Xray.sh
@@ -220,7 +220,7 @@ cat > /etc/nginx/atrandys/ws_default.conf<<-EOF
 server {
         listen 80;
         server_name $your_domain;
-        rewrite ^(.*)$ https://${server_name}$1 permanent;
+        rewrite ^(.*)$ https://\${server_name}\$1 permanent;
 }
 server {
         listen 443 ssl http2 so_keepalive=on;
@@ -232,17 +232,17 @@ server {
         ssl_certificate /usr/local/etc/xray/cert/fullchain.cer;
         ssl_certificate_key /usr/local/etc/xray/cert/private.key;
         ssl_protocols TLSv1.2 TLSv1.3;
-        ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305>
+        ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
 
         client_header_timeout 1071906480m;
         client_body_buffer_size 512k;
         keepalive_timeout 1071906480m;
         location /$your_domain {
-                if ($content_type !~ "application/grpc") {
+                if (\$content_type !~ "application/grpc") {
                         return 404;
                 }
                 client_max_body_size 0;
-                grpc_set_header X-Real-IP $proxy_add_x_forwarded_for;
+                grpc_set_header X-Real-IP \$proxy_add_x_forwarded_for;
                 client_body_timeout 1071906480m;
                 grpc_read_timeout 1071906480m;
                 grpc_pass grpc://127.0.0.1:2002;
