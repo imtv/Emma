cfc852763bb12dbc496b3534be4eac5a6d996f5b
imtv <59756626+imtv@users.noreply.github.com>
2022-03-10 10:27:55 +0800

    Update Xray.sh

--- Xray.sh
+++ Xray.sh
@@ -217,28 +217,36 @@ EOF
 
 newpath=$(cat /dev/urandom | head -1 | md5sum | head -c 4)
 cat > /etc/nginx/atrandys/ws_default.conf<<-EOF
-server { 
-    listen 80;
-    listen 443 ssl http2;
-    server_name $your_domain;
-    index index.html;
-    root /usr/share/nginx/html;
+server {
+        listen 80;
+        server_name $your_domain;
+        rewrite ^(.*)$ https://${server_name}$1 permanent;
+}
+server {
+        listen 443 ssl http2 so_keepalive=on;
+        server_name $your_domain;
 
-    ssl_certificate /usr/local/etc/xray/cert/fullchain.cer;
-    ssl_certificate_key /usr/local/etc/xray/cert/private.key;
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+        index index.html;
+        root /usr/share/nginx/html;
 
-    location /$your_domain {
+        ssl_certificate /usr/local/etc/xray/cert/fullchain.cer;
+        ssl_certificate_key /usr/local/etc/xray/cert/private.key;
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305>
+
+        client_header_timeout 1071906480m;
+        client_body_buffer_size 512k;
+        keepalive_timeout 1071906480m;
+        location /$your_domain {
+                if ($content_type !~ "application/grpc") {
+                        return 404;
+                }
                 client_max_body_size 0;
-                keepalive_requests 4294967296;
+                grpc_set_header X-Real-IP $proxy_add_x_forwarded_for;
                 client_body_timeout 1071906480m;
-                send_timeout 1071906480m;
-                lingering_close always;
                 grpc_read_timeout 1071906480m;
-                grpc_send_timeout 1071906480m;
                 grpc_pass grpc://127.0.0.1:2002;
-    }
+        }
 }
 EOF
 
